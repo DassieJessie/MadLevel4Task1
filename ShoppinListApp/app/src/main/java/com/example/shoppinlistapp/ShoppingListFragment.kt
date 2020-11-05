2d106e728395574ae867b41b844d85cbdf6d2137
Jienie12 <jr.egmond@gmail.com>
2020-11-05 10:43:55 +0100

    3.2 Display products from database

--- ShoppinListApp/app/src/main/java/com/example/shoppinlistapp/ShoppingListFragment.kt
+++ ShoppinListApp/app/src/main/java/com/example/shoppinlistapp/ShoppingListFragment.kt
@@ -12,6 +12,10 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_shopping_list.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -19,6 +23,7 @@ import kotlinx.android.synthetic.main.fragment_shopping_list.*
 class ShoppingListFragment : Fragment() {
 
     private lateinit var productRepository: ProductRepository
+    private val mainScope = CoroutineScope(Dispatchers.Main)
 
     private val products = arrayListOf<Product>()
     private val productAdapter = ProductAdapter(products)
@@ -48,5 +53,22 @@ class ShoppingListFragment : Fragment() {
         )
 
     }
+
+    private fun getShoppingListFromDatabase() {
+        mainScope.launch {
+            //get all the reminders from the database
+            val products = withContext(Dispatchers.IO) {
+                productRepository.getAllProducts()
+            }
+            //clears current reminders list
+            this@ShoppingListFragment.products.clear()
+            //adds all the reminders from the database to the list
+            this@ShoppingListFragment.products.addAll(products)
+
+
+            productAdapter.notifyDataSetChanged()
+        }
+    }
+
 
 }

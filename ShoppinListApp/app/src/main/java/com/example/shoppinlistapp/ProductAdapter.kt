82b18ddae905abb9a89f63eb50a4e659465aa717
Jienie12 <jr.egmond@gmail.com>
2020-11-05 10:14:08 +0100

    (Adding RV adapter)

---
+++ ShoppinListApp/app/src/main/java/com/example/shoppinlistapp/ProductAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.shoppinlistapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_product.view.*
+
+abstract class ProductAdapter(private val products: List<Product>) :
+    RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun databind(product: Product) {
+            itemView.tvProductName.text = product.productName
+            itemView.tvProductQuantity.text = product.productQuantity.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_product, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return products.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(products[position])
+    }
+}
